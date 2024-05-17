2a0a75f6ca500fdf896c36d552ff990d29bbb348
Bin Zhang <bin.zhang@character.ai>
2024-05-16 22:24:42 -0700

    archive link

--- page.js
+++ page.js
@@ -1,13 +1,36 @@
-// redirect archives link to open in browser
-if (location.href.indexOf('/archives/') >= 0) {
-  let links = document.querySelectorAll('.c-link')
-  for (let x of links) {
-	if (x.innerText === "open this link in your browser") {
-	  x.click()
-	  break
+function removeElement(sel) {
+	for (const el of document.querySelectorAll(sel)) {
+		el.parentElement.removeChild(el)
 	}
-  }
 }
 
-// tweak ui
-document.querySelector("body").style.fontFamily = "sf pro rounded"
+function openArchiveInBrowser() {
+	// redirect archives link to open in browser
+	if (location.href.indexOf('/archives/') < 0) {
+		return
+	}
+	let links = document.querySelectorAll('.c-link')
+	for (let x of links) {
+		if (x.innerText === 'open this link in your browser') {
+			x.click()
+			break
+		}
+	}
+}
+
+function setFont() {
+	document.querySelector('body').style.fontFamily = 'sf pro'
+}
+
+var intervalID
+
+function init() {
+	console.log('tweakslack')
+	setFont()
+	openArchiveInBrowser()
+	const banner = '.p-client__banners'
+	removeElement(banner)
+	intervalID = setInterval(removeElement, 11*1000, banner)
+}
+
+init()
